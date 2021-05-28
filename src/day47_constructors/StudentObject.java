d0067696c7693b496b84eb5135500b0b84823b6f
Aigerim Ingco <aikish20@yahoo.com>
2021-05-28 08:08:05 +0600

    added new student class for constructor

---
+++ src/day47_constructors/StudentObject.java
@@ -0,0 +1,12 @@
+package day47_constructors;
+
+public class StudentObject {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3 = new Student("Aigerim");
+        Student st4 = new Student("Nazar");
+        Student st5= new Student("Ayana",15);
+
+    }
+}
