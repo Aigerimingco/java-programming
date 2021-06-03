db645a19fbde224e86e61bdbc591b16efc2df223
Aigerim Ingco <aikish20@yahoo.com>
2021-06-04 05:15:01 +0600

    day 49 static method

---
+++ src/day49_static/StaticVariablesTest.java
@@ -0,0 +1,12 @@
+package day49_static;
+
+public class StaticVariablesTest {
+    public static void main(String[] args) {
+        System.out.println("StaticVariables.name = " + StaticVariables.name);
+        System.out.println("StaticVariables.userNme = " + StaticVariables.userName);
+        System.out.println("StaticVariables.age = " + StaticVariables.age);
+        System.out.println("StaticVariables.price = " + StaticVariables.price);
+        System.out.println(Locators.userName);
+    }
+    
+}
