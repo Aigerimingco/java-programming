53fdd712559ea795873184acd54ba2ed06474501
Aigerim Ingco <aikish20@yahoo.com>
2021-06-12 22:11:55 +0600

    Rules of overriding method

---
+++ src/day52_inheritance/Instagram.java
@@ -0,0 +1,12 @@
+package day52_inheritance;
+
+public class Instagram extends MobileApp{
+    public void postPhoto() {
+        System.out.println("Posting photo on Instagram");
+    }
+        @Override
+        public void useTheApp ( int minutes){
+            System.out.println("Using instagram app features");
+            postPhoto();
+        }
+    }
--- src/day52_inheritance/MobileApp.java
+++ src/day52_inheritance/MobileApp.java
@@ -1,6 +1,11 @@
 package day52_inheritance;
 
 public class MobileApp {
-    String name, version;
+    String name;
+    double version;
+
+    public  void useTheApp(int minutes){
+        System.out.println("Using "+name+" app for "+minutes+" minutes");
+    }
 
 }
