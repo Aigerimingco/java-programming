c05c26aa58a9f6e59b5183b2aecce5ea2db995ac
Aigerim Ingco <aikish20@yahoo.com>
2021-06-22 23:45:19 +0600

    added some notes

--- src/day55_abstraction/exercise_example/Exercise.java
+++ src/day55_abstraction/exercise_example/Exercise.java
@@ -4,9 +4,14 @@ public abstract class Exercise {
     public  void start(){
         System.out.println("Warming up and starting the exercise");
     }
-
+    // abstract method-without body/implementation, just signature
     public abstract void perform();
 
+    /**
+     * another abstract method that concrete sub classes will override/implement
+     * @param minutes- how long is exercise
+     * @return number of calories burned/used
+     */
     public abstract int getCaloriesCount(int minutes);
 
 }
