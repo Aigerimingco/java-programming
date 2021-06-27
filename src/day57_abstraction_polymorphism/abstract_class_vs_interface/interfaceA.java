e65e475429d0c6eebea559facf73c0f802cf2af5
Aigerim Ingco <aikish20@yahoo.com>
2021-06-27 23:14:20 +0600

    difference between abstract vs interface classes

--- src/day57_abstraction_polymorphism/abstract_class_vs_interface/interfaceA.java
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/interfaceA.java
@@ -1,7 +1,15 @@
 package day57_abstraction_polymorphism.abstract_class_vs_interface;
 
 public interface interfaceA {
-public abstract void absMethodD(int num);
+    /**
+     * interfaces can ONLY have PUBLIC STATIC FINAL variables
+     */
+    public static final String TYPE= "interface";
+    double MAX_COUNT= 500;//This is also public static final
+
+
+
+    public abstract void absMethodD(int num);
 public static void staticMethodE(String str){
     /**
      * starting from java 8(jdk 1.8
