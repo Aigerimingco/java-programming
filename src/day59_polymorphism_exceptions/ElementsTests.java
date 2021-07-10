ddbfc1ee63ddda7c26e0d651b7213d42dea6eb19
Aigerim Ingco <aikish20@yahoo.com>
2021-07-11 03:26:55 +0600

    Polymorphism Exceptions

--- src/day59_polymorphism_exceptions/ElementsTests.java
+++ src/day59_polymorphism_exceptions/ElementsTests.java
@@ -11,5 +11,16 @@ link.getLinkHref();
 WebElement loginLink= new Link();
 loginLink.click();
 
+WebElement username= new InputField();
+username.sendKeys("wooden spoon");
+username.click();
+
+WebElementUtil.clickElement(loginLink);
+WebElementUtil.clickElement(username);
+//WebElementUtil.clickElement(new String("hello")); String is not child of WebElement
+
+
+       WebElement login= WebElementUtil.getLinkWithText("login");
+       login.click();
     }
 }
