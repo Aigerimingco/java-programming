b027c737366352cf5b44b4361423a66e64e98c12
Aigerim Ingco <aikish20@yahoo.com>
2021-07-11 18:38:24 +0600

    Exception error handling

--- src/OfficeHours/quizReview.java
+++ src/OfficeHours/quizReview.java
@@ -6,11 +6,6 @@ import java.util.WeakHashMap;
 
 public abstract class quizReview {
     public abstract void eat();
-}
-class Bird extends quizReview implements CanFly{
-
-    @Override
-    public void eat() {
         
     }
-}
+
