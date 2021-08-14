13455704a6960806b669ffc34955ae8b92ea279d
Aigerim Ingco <aikish20@yahoo.com>
2021-08-14 13:17:31 -0600

    Map Examples

--- src/day63_collections/MapExamples2.java
+++ src/day63_collections/MapExamples2.java
@@ -1,7 +1,9 @@
 package day63_collections;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExamples2 {
     public static void main(String[] args) {
@@ -10,11 +12,59 @@ public class MapExamples2 {
         Map<Integer, Person> map= new HashMap<>();
         map.put(32, new Person("MJ", 32));
         map.put(null, new Person("",0));
+        map.put(12, new Person("Nick", 10));
 
 
-        System.out.println(map.get(31));
-        System.out.println(map.get(32));
-        System.out.println(map.get(null));
+        System.out.println(map.get(31));//null
+        System.out.println(map.get(32));//Person{name='MJ', age=32}
+        System.out.println(map.get(null));//Person{name='', age=0}
+
+        System.out.println(map);
+
+
+        Map<Integer, Person> map2= new LinkedHashMap<>();
+        map2.put(32, new Person("MJ", 32));
+        map2.put(null, new Person("",0));
+        map2.put(12, new Person("Nick", 10));
+        System.out.println(map2);
+
+
+        Map<Integer, Person> map3= new TreeMap<>();
+        map3.put(32, new Person("MJ", 32));
+        map3.put(0, new Person("",0));
+        map3.put(-12, new Person("Nick", 10));
+        System.out.println(map3);
+
+
+
+        /*
+        DB
+        FIRST_NAME James
+        LAST_NAME Bond
+        AGE 50
+
+        DB
+        FIRST_NAME Amy
+        LAST_NAME Rose
+        AGE 30
+
+        Map<String, String>map= new HashMap<>();
+        map.put("FIRST_NAME", "James");
+        map.put("LAST_NAME", "Bond");
+
+        map.get(FIRST_NAME);
+       if( map.get("AGE")< 0) Fail test case
+
+
+       List< Map<String, String> > list
+
+       list.get(0);-> Map<String, String.->(James Bond)
+       list.get(0).get("FIRST_NAME") ->(James)
+
+
+         */
+
+
 
     }
 }
